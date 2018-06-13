93e9253febc14b17d17a5fbc2eb0e22f1c974083
neilswainston <neil.swainston@manchester.ac.uk>
2018-06-13 20:16:43 +0100

    Updated logo draw.

--- genegenie/admin/logo.py
+++ genegenie/admin/logo.py
@@ -13,8 +13,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def draw_logo(filename='logo.png'):
+def draw_logo(filename='logo.png', show_text=True, dpi=800):
     '''Draws logo.'''
+
+    # Clear:
+    plt.clf()
 
     # Plot curves:
     variance = 0.05
@@ -27,20 +30,21 @@ def draw_logo(filename='logo.png'):
     # Plot line:
     plt.plot(np.linspace(-1, 5, 100), [0] * 100, linewidth=4, color='#010101')
 
-    # Write text:
-    y_offset = -0.33
-    fontsize = 12
+    if show_text:
+        # Write text:
+        y_offset = -0.33
+        fontsize = 12
 
-    for idx, c in enumerate('GENEGENIE'):
-        plt.text((idx - 0.8) * 0.6, y_offset, c, color='#010101',
-                 fontsize=fontsize)
+        for idx, c in enumerate('GENEGENIE'):
+            plt.text((idx - 0.8) * 0.6, y_offset, c, color='#010101',
+                     fontsize=fontsize)
 
     # Clean:
     plt.axis('off')
 
     # Save:
-    plt.savefig(filename, transparent=True, bbox_inches='tight',
-                pad_inches=0.1)
+    plt.savefig(filename, transparent=False, bbox_inches='tight',
+                pad_inches=0.1, dpi=dpi)
 
 
 def plot(mu, variance, color, scale=1):
@@ -53,6 +57,7 @@ def plot(mu, variance, color, scale=1):
 def main():
     '''main method.'''
     draw_logo()
+    draw_logo(filename='logo_no_text.png', show_text=False)
 
 
 if __name__ == '__main__':
